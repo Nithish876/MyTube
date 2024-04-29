6bd021af5d9462f3e75e9dbc44fa22fc79b57386
= <=>
2024-04-29 06:32:12 +0530

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className=' p-4 bg-emerald-500 '>
+    <h1 className='text-3xl font-bold '>MyTube</h1>
+    </div>)
+}
+
+export default App
